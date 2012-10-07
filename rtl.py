ddd1b94cc3ea9d48c7b2647fb98c4c5f7fe20e03
Praveen Vijayan <praveenv.vijayan@gmail.com>
2012-10-08 00:52:05 +0530

    Fixed Arabic broken text

--- rtl.py
+++ rtl.py
@@ -1,10 +1,12 @@
 import sublime, sublime_plugin, sys
 sys.path.append( 'lang' )
+import arabic_reshaper
 from algorithm import get_display
 
 class bidiCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		region = sublime.Region(0, self.view.size())
 		txt = self.view.substr(region)
-		bdiText = get_display(txt, upper_is_rtl=True)
+		reshaped_text = arabic_reshaper.reshape(txt)
+		bdiText = get_display(reshaped_text)
 		self.view.replace(edit, region, bdiText)
